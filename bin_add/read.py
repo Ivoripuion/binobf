aabbaac80b16f4c213398a6e18ba0b95ad7e8463
atlanta_zhao <2510061676@qq.com>
2019-11-25 18:39:20 +0800

    delete all

--- bin_add/read.py
+++
@@ -1,5 +0,0 @@
-import os
-
-files=os.listdir("./malware_aftercomp/")
-for file in files:
-	print(file)
--- bin_add/workstation/README.md
+++
@@ -1,2 +0,0 @@
-﻿# 程式使用
-在sample下的test文件夹中存放样本，然后运行Demo文件夹下的usedemo文件，即可生成10个随机样本在sample/aftertest目录中。
